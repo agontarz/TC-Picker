50efb132e84f0e5302f8f9f27e7af6dd754d0b7c
unknown <gontarz@live.ca>
2020-10-19 15:47:46 -0400

    “test3”

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 import pandas as pd
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-df = pd.read_excel('C:/Users/Aleks/Google Drive/LongMill/Design/ToolChain-Picker/Code/mysite/myapp/SoftwareList.xlsx')
+
+#df = pd.read_excel('C:/Users/Aleks/Google Drive/LongMill/Design/ToolChain-Picker/Code/mysite/myapp/SoftwareList.xlsx')
+df = pd.read_excel(BASE_DIR+"/static/myapp/SoftwareList.xlsx")
 softReturn = df['Name'].loc[df['Difficulty'] == 1]
 print(softReturn)
 
